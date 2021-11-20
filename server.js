1d2e1a8719e2e7bcc5a3f1701a9e44b79ed66f9b
Zaid Abdalkarim <64388455+Zaid-Abdalkarim@users.noreply.github.com>
2021-11-20 10:09:10 -0800

    port num is debugged.

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ require("./app/routes/user.routes")(app);
 require("./app/routes/image.routes")(app);
 
 const PORT = process.env.PORT || 8080;
+console.log("PORT is " + PORT);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
