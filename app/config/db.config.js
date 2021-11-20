3beb7c58d1d84f305b39ab8011282fee3fce906b
Zaid Abdalkarim <64388455+Zaid-Abdalkarim@users.noreply.github.com>
2021-11-20 11:16:17 -0800

    changed db.config.js again

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: "pintrest-spa.database.windows.net",
-  port: '1433',
-  USER: "zaidabdalkarim",
-  PASSWORD: "Azswsixllo9.",
-  DB: "newdb",
+  HOST: "ec2-35-168-80-116.compute-1.amazonaws.com",
+  USER: "dikzkzzbewnldd",
+  PORT: '5432',
+  PASSWORD: "a8abf15cdb1c72fdebde9d821ce04de5a59bcd38c457f22c78bb9af76af888a6",
+  DB: "d1mn79bsnmcug7",
   dialect: "mysql",
   pool: {
     max: 5,
