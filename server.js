79932935e9da30c9cccc54611e695df59d243854
Zaid Abdalkarim <64388455+Zaid-Abdalkarim@users.noreply.github.com>
2021-12-06 21:28:17 -0800

    updating everything

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
 
-// db.sequelize.sync(); prod
+db.sequelize.sync();
 
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-}); // dev
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// }); // dev
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
